f6ddd32bd82bd328002d30e6f4643fa94f43a3f2
vvworm <vvworm@gmail.com>
2012-03-03 20:09:56 +0800

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TTest::Application.config.secret_token = 'ffb4317290f928d331bb148d6b1c47ec63c84c6daddef9ea66970fdca7b0be02ed7b040bcb019c78d9e08bbbdb346f0402071ddd0fa448bc418ded207ef274fe'
